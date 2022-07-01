c8af34aac63fcdc3b175a60503012ce632b77c1c
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2022-07-01 09:45:15 +0500

    Add files via upload

---
+++ Korzina/js/script.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let products = document.querySelectorAll(".product"),
+    buttons = document.querySelectorAll("button"),
+    openBtn = document.querySelector(".open");
+
+  function creatCart() {
+    let cart = document.createElement("div"),
+      field = document.createElement("div"),
+      heading = document.createElement("h2"),
+      closeBtn = document.createElement("button");
+
+    cart.classList.add("cart");
+    field.classList.add("cart-field");
+    closeBtn.classList.add("close");
+
+    heading.textContent = "Our Korzina";
+    closeBtn.textContent = "Close";
+
+    document.body.appendChild(cart);
+    cart.appendChild(heading);
+    cart.appendChild(closeBtn);
+    cart.appendChild(field);
+  }
+  creatCart();
+
+  // Cart //
+  let cart = document.querySelector(".cart"),
+    closeBtn = document.querySelector(".close"),
+    field = document.querySelector(".cart-field");
+
+  openBtn.addEventListener("click", () => {
+    cart.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    cart.style.display = "none";
+  });
+
+  buttons.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      let cloneItem = products[i].cloneNode(true),
+        btn = cloneItem.querySelector("button");
+
+      btn.addEventListener("click", () => {
+        cloneItem.style.display = "none";
+      });
+
+      btn.textContent = "Delete";
+
+      field.appendChild(cloneItem);
+      products[i].remove();
+    });
+  });
+});
